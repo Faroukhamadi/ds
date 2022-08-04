80ecd3ebd5d30baaec2db80db901c73f58387abb
Faroukhamadi <faroukhamadi1@outlook.com>
2022-08-04 21:43:25 +0100

    add print to binary tree

---
+++ bt/bt.go
@@ -0,0 +1,41 @@
+package bt
+
+import (
+	"fmt"
+
+	"golang.org/x/exp/constraints"
+)
+
+type TreeNode[T constraints.Ordered] struct {
+	Val   T
+	Left  *TreeNode[T]
+	Right *TreeNode[T]
+}
+
+func New[T constraints.Ordered](val T) *TreeNode[T] {
+	return &TreeNode[T]{Val: val}
+}
+
+func Invert[T constraints.Ordered](root *TreeNode[T]) *TreeNode[T] {
+	if root == nil {
+		return nil
+	}
+
+	node := TreeNode[T]{Val: root.Val, Left: Invert(root.Right), Right: Invert(root.Left)}
+	return &node
+}
+
+func Print[T constraints.Ordered](root *TreeNode[T], level int) {
+	if level == 0 {
+		fmt.Println("BINARY TREE")
+	}
+	if root != nil {
+		Print(root.Right, level+1)
+		prefix := ""
+		for i := 0; i < level; i++ {
+			prefix += "     "
+		}
+		fmt.Println(prefix+"->", root.Val)
+		Print(root.Left, level+1)
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"github.com/Faroukhamadi/ds/bt"
+)
+
 func main() {
-	println("Hello, Main!")
+	node := bt.New(1)
+	node.Left = bt.New(2)
+	node.Right = bt.New(3)
+	node.Left.Left = bt.New(4)
+	node.Right.Right = bt.New(5)
+
+	bt.Print(node, 0)
+
+	bt.Invert(node)
+
+	bt.Print(node, 0)
+
 }
