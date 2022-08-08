18ae2e50cf7f0cda227a318e432d93e9bd0a4bf1
faroukhamadi <faroukhamadi1@outlook.com>
2022-08-08 19:57:12 +0100

    add max depth

--- main.go
+++ main.go
@@ -7,8 +7,22 @@ import (
 )
 
 func main() {
-	p := bt.New(1)
-	q := bt.New(1)
 
-	fmt.Println(bt.IsSameTree(p, q))
+	p := bt.New(1)
+	q := bt.New(2)
+	x := bt.New(3)
+	y := bt.New(4)
+	a := bt.New(5)
+	b := bt.New(6)
+
+	p.Left = q
+	q.Left = x
+	x.Left = y
+	y.Left = a
+	a.Right = b
+	b.Left = nil
+
+	bt.Print(p, 0)
+
+	fmt.Println("max depth is: ", bt.MaxDepth(p))
 }
