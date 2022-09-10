ea346bbc070a53f693097689b9c8e2df684e2c58
Faroukhamadi <faroukhamadi1@outlook.com>
2022-09-10 21:16:52 +0100

    add hash interface

--- hm/hm.go
+++ hm/hm.go
@@ -2,40 +2,43 @@ package hm
 
 import (
 	"math/big"
-	"reflect"
 )
 
-type HashMap[K string | int, T string | int] struct {
-	Entries    int
+type String string
+type Int int
+
+type Key interface {
+	*String | *Int
+	Hash(int) int
+}
+
+type HashMap[K Key, T string | int] struct {
+	entries    int
 	loadFactor float64
 	capacity   int
 	arr        []int
 }
 
-func New[K, V string | int]() *HashMap[K, V] {
-	return &HashMap[K, V]{Entries: 0, loadFactor: 0.75, capacity: 16, arr: make([]int, 16)}
+func New[K Key, V string | int]() *HashMap[K, V] {
+	return &HashMap[K, V]{entries: 0, loadFactor: 0.75, capacity: 16, arr: make([]int, 16)}
 }
 
-func (h *HashMap[K, V]) Set(key K, val V) {
-	var index int
-	if reflect.TypeOf(key).Kind() == reflect.Int {
-		index = HashInt(key, h.capacity)
-	}
+func Hash[T Key](key T, capacity int) int {
+	return key.Hash(capacity)
 }
 
-// Increase size when Entries = capacity * loadFactor
-func HashInt(key, capacity int) int {
-	return key % capacity
+func (key *Int) Hash(capacity int) int {
+	return int(*key) % capacity
 }
 
-func Hash(key string, capacity int) int {
+func (key *String) Hash(capacity int) int {
 	hexValueBig := new(big.Int)
 	hexValueBig.SetString("F0000000L", 16)
 
 	hexValueInt := hexValueBig.Int64()
 
 	h := 0
-	for _, p := range key {
+	for _, p := range *key {
 		h = (h << 4) + int(p)
 		g := h & int(hexValueInt)
 		if g != 0 {
@@ -45,3 +48,9 @@ func Hash(key string, capacity int) int {
 	}
 	return h % capacity
 }
+
+func (h *HashMap[K, V]) Set(key K, val V) {
+	index := key.Hash(h.capacity)
+}
+
+// Increase size when Entries = capacity * loadFactor
